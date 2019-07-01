1fd4e977e626e5842bb9ad9a4c74744a885eea35
Anvar Nazarov <roman_emperor5@mail.ru>
2019-07-01 20:26:46 +0500

    Раскомментировал код.

--- js/lesson3.js
+++ js/lesson3.js
@@ -4,32 +4,32 @@
 /* 1) Спрашиваем у пользователя “Ваш месячный доход?” и результат сохраняем в переменную money
 */
 
-// let money = +prompt('Ваш месячный доход?');
+let money = +prompt('Ваш месячный доход?');
 
-// console.log(money);
+console.log(money);
 
 /*  2) Спросить у пользователя “Перечислите возможные расходы за рассчитываемый период через запятую”
 сохранить в переменную addExpenses, вывести в консоль в виде массива
 */
 
-// let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую?');
-// console.log(addExpenses.split(', '));
+let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую?');
+console.log(addExpenses.split(', '));
 
 /* 3) Спросить у пользователя “Есть ли у вас депозит в банке?” и сохранить данные
 в переменной deposit (булевое значение true/false)
  */
 
-// let deposit = confirm('Есть ли у вас депозит в банке?');
-// console.log(deposit);
+let deposit = confirm('Есть ли у вас депозит в банке?');
+console.log(deposit);
 
 /* 4) Вывести в консоль типы данных money, income, deposit
  */
 
-// let income = '10000';
-//
-// console.log(typeof money);
-// console.log(typeof income);
-// console.log(typeof deposit);
+let income = '10000';
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
 
 /* 5) Спросить у пользователя по 2 раза каждый вопрос и записать ответы в переменные
 
@@ -40,33 +40,33 @@
 в итоге 4 вопроса и 4 переменных
 */
 
-// let questionOne = prompt('Какие обязательные ежемесячные расходы у вас есть?');
-// let questionTwo = prompt('Какие обязательные ежемесячные расходы у вас есть?');
-// let questionThree = prompt('Во сколько это обойдется?');
-// let questionFour = prompt('Во сколько это обойдется?');
-// console.log(questionOne);
-// console.log(questionTwo);
-// console.log(questionThree);
-// console.log(questionFour);
+let questionOne = prompt('Какие обязательные ежемесячные расходы у вас есть?');
+let questionTwo = prompt('Какие обязательные ежемесячные расходы у вас есть?');
+let questionThree = prompt('Во сколько это обойдется?');
+let questionFour = prompt('Во сколько это обойдется?');
+console.log(questionOne);
+console.log(questionTwo);
+console.log(questionThree);
+console.log(questionFour);
 
 /*  6) Вычислить доход за месяц, учитывая обязательные расходы,
 сохранить в переменную budgetMonth и вывести результат в консоль
  */
 
-// let expenditureOne = 1300,
-//     expenditureTwo = 627;
-//
-// let netMonthIncome = money - expenditureOne - expenditureTwo;
-// console.log(netMonthIncome);
+let expenditureOne = 1300,
+    expenditureTwo = 627;
+
+let netMonthIncome = money - expenditureOne - expenditureTwo;
+console.log(netMonthIncome);
 
 /* 7) Зная budgetMonth, посчитать за сколько месяцев будет достигнута цель mission,
 вывести в консоль, округляя в большую сторону
  */
 
 let budgetMonth = 25000;
-// let mission = 120000;
-// let result = Math.ceil(mission/budgetMonth);
-// console.log(result);
+let mission = 120000;
+let result = Math.ceil(mission/budgetMonth);
+console.log(result);
 
 /* 8) Поправить budgetDay учитывая бюджет на месяц, а не месячный доход.
 Вывести в консоль  округлив в меньшую сторону (методы объекта Math в помощь)
